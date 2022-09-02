f3492ebfacf893c4ada5ceea6f41e2245315b252
Lucas <lugablima@gmail.com>
2022-09-02 10:21:14 -0300

    chore: add typing to cardsService.ts file

--- src/services/cardsService.ts
+++ src/services/cardsService.ts
@@ -7,14 +7,14 @@ import * as employeeRepository from "../repositories/employeeRepository";
 import * as cardRepository from "../repositories/cardRepository";
 
 function generateNameOnCard(fullName: string): string {
-    const arrayOfTheName = fullName.toUpperCase().split(" ");
+    const arrayOfTheName: string[] = fullName.toUpperCase().split(" ");
 
     if(arrayOfTheName.length === 1) return arrayOfTheName[0]; 
 
-    let nameOnCard = arrayOfTheName[0];
+    let nameOnCard: string = arrayOfTheName[0];
 
     for(let i = 1; i < arrayOfTheName.length - 1; i++) {
-        const middleName = arrayOfTheName[i];  
+        const middleName: string = arrayOfTheName[i];  
 
         if(middleName.length >= 3) {
             nameOnCard += " " + middleName[0];
@@ -31,33 +31,33 @@ export async function createCard(data: { employeeId: number, type: cardRepositor
         throw { code: "Error_Api_Key_Not_Sent", message: "The api key was not sent" };
     }
     
-    const company = await companyRepository.findByApiKey(apiKey); 
+    const company: companyRepository.Company | undefined = await companyRepository.findByApiKey(apiKey); 
 
     if(!company) {
         throw { code: "Error_Invalid_Api_Key", message: "The api key is invalid" };
     }
 
-    const employee = await employeeRepository.findById(data.employeeId); 
+    const employee: employeeRepository.Employee | undefined = await employeeRepository.findById(data.employeeId); 
 
     if(!employee) {
         throw { code: "Error_Invalid_Employee", message: "This employee is not registered" };
     }
 
-    const employeeCards = await cardRepository.findByTypeAndEmployeeId(data.type, data.employeeId);
+    const employeeCards: cardRepository.Card | undefined = await cardRepository.findByTypeAndEmployeeId(data.type, data.employeeId);
 
     if(employeeCards) {
         throw { code: "Error_Card_Type_Conflict", message: "There is already a card of this type registered for this user" };
     }
 
-    const cardNumber = faker.finance.account(16);
-    const nameOnCard = generateNameOnCard(employee.fullName);
-    const expirationDate = dayjs().add(5, "year").format("MM/YY");
-    const cvc = faker.finance.creditCardCVV();
+    const cardNumber: string = faker.finance.account(16);
+    const nameOnCard: string = generateNameOnCard(employee.fullName);
+    const expirationDate: string = dayjs().add(5, "year").format("MM/YY");
+    const cvc: string = faker.finance.creditCardCVV();
 
-    const cvcSecretKey = process.env.CVC_SECRET_KEY ? process.env.CVC_SECRET_KEY : "secret"; 
+    const cvcSecretKey: string = process.env.CVC_SECRET_KEY ? process.env.CVC_SECRET_KEY : "secret"; 
 
-    const cryptr = new Cryptr(cvcSecretKey);
-    const encryptedCvc = cryptr.encrypt(cvc);
+    const cryptr: Cryptr = new Cryptr(cvcSecretKey);
+    const encryptedCvc: string = cryptr.encrypt(cvc);
     // const decryptedCvc = cryptr.decrypt(encryptedCvc);
     
     const card: cardRepository.CardInsertData = {
