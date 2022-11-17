bedd7e27e097754b91f0b9798ac57811eea37058
Lucas <lugablima@gmail.com>
2022-11-17 15:31:47 -0300

    refactor: modify the title of generated errors from code to name

--- src/middlewares/errorHandlerMiddleware.ts
+++ src/middlewares/errorHandlerMiddleware.ts
@@ -1,30 +1,35 @@
 import { Request, Response, NextFunction } from "express";
+import { ApplicationError } from "utils/errorHandlingUtils";
 
-// eslint-disable-next-line @typescript-eslint/no-unused-vars
-export default function errorHandlerMiddleware(error: any, req: Request, res: Response, _next: NextFunction) {
-	if (error.code === "NotSent") {
+export default function errorHandlerMiddleware(
+	error: ApplicationError | Error,
+	req: Request,
+	res: Response,
+	_next: NextFunction
+) {
+	if (error.name === "NotSent") {
 		return res.status(400).send(error.message);
 	}
 
 	if (
-		error.code === "Invalid" ||
-		error.code === "Expired" ||
-		error.code === "NotActivated" ||
-		error.code === "Insufficient" ||
-		error.code === "DifferentTypes"
+		error.name === "Invalid" ||
+		error.name === "Expired" ||
+		error.name === "NotActivated" ||
+		error.name === "Insufficient" ||
+		error.name === "DifferentTypes"
 	) {
 		return res.status(401).send(error.message);
 	}
 
-	if (error.code === "NotFound") {
+	if (error.name === "NotFound") {
 		return res.status(404).send(error.message);
 	}
 
 	if (
-		error.code === "TypeConflict" ||
-		error.code === "Activated" ||
-		error.code === "Blocked" ||
-		error.code === "Unlocked"
+		error.name === "TypeConflict" ||
+		error.name === "Activated" ||
+		error.name === "Blocked" ||
+		error.name === "Unlocked"
 	) {
 		return res.status(409).send(error.message);
 	}
--- src/utils/errorHandlingUtils.ts
+++ src/utils/errorHandlingUtils.ts
@@ -2,50 +2,51 @@ export type ApplicationError = {
 	name: string;
 	message: string;
 };
+
 export function notSend(entity: string) {
-    return { code: "NotSent", message: `${entity} not sent!` };
+	return { name: "NotSent", message: `${entity} not sent!` };
 }
 
 export function notFound(entity: string) {
-    return { code: "NotFound", message: `${entity} not found!` };
+	return { name: "NotFound", message: `${entity} not found!` };
 }
 
 export function invalid(entity: string) {
-    return { code: "Invalid", message: `Invalid ${entity}!` };
+	return { name: "Invalid", message: `Invalid ${entity}!` };
 }
 
 export function expired(entity: string) {
-    return { code: "Expired", message: `Expired ${entity}!` };
+	return { name: "Expired", message: `Expired ${entity}!` };
 }
 
 export function notActivated(entity: string) {
-    return { code: "NotActivated", message: `${entity} not activated!` };
+	return { name: "NotActivated", message: `${entity} not activated!` };
 }
 
 export function activated(entity: string) {
-    return { code: "Activated", message: `Activated ${entity}!` };
+	return { name: "Activated", message: `Activated ${entity}!` };
 }
 
 export function blocked(entity: string) {
-    return { code: "Blocked", message: `Blocked ${entity}!` };
+	return { name: "Blocked", message: `Blocked ${entity}!` };
 }
 
 export function unlocked(entity: string) {
-    return { code: "Unlocked", message: `${entity} unlocked!` };
+	return { name: "Unlocked", message: `${entity} unlocked!` };
 }
 
 export function insufficient(entity: string) {
-    return { code: "Insufficient", message: `Insufficient ${entity}!` };
+	return { name: "Insufficient", message: `Insufficient ${entity}!` };
 }
 
 export function typeConflict(entity: string) {
-    return { code: "TypeConflict", message: `${entity} has a type conflict!` };
+	return { name: "TypeConflict", message: `${entity} has a type conflict!` };
 }
 
 export function differentTypes(entities: string) {
-    return { code: "DifferentTypes", message: `${entities} are of different types!` };
+	return { name: "DifferentTypes", message: `${entities} are of different types!` };
 }
 
 export function notRegistered(entity: string) {
-    return { code: "NotRegistered", message: `There is no registered ${entity}!` };
+	return { name: "NotRegistered", message: `There is no registered ${entity}!` };
 }
