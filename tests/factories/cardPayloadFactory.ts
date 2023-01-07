7db35f761932164145d7a384036063ad93f146e7
Lucas <lugablima@gmail.com>
2023-01-07 11:37:47 -0300

    test: add activate function in the card payload factory

--- tests/factories/cardPayloadFactory.ts
+++ tests/factories/cardPayloadFactory.ts
@@ -1,12 +1,24 @@
-import { CreateCardSchema } from "../../src/types/cardsTypes";
+import { CreateCardSchema, ActivateCardSchema } from "../../src/types/cardsTypes";
 import * as faker from "../helpers/faker";
+import { decryptCvc } from "../../src/utils/cardsUtils";
 
-type OverwriteCardsPayload = Partial<CreateCardSchema>;
+type OverwriteCreateCardPayload = Partial<CreateCardSchema>;
 
-export function cardPayloadFactory(overwrite: OverwriteCardsPayload = {}): CreateCardSchema {
+type OverwriteActivateCardPayload = Partial<ActivateCardSchema>;
+
+export function create(overwrite: OverwriteCreateCardPayload = {}): CreateCardSchema {
 	return {
 		employeeId: faker.generateRandomNumber({ min: 1 }),
 		type: faker.generateRandomTransactionType(),
+		...overwrite,
+	};
+}
+
+export function activate(overwrite: OverwriteActivateCardPayload = {}): ActivateCardSchema {
+	return {
+		cardId: faker.generateRandomNumber({ min: 1 }),
+		cvc: decryptCvc(faker.generateSecurityCode()),
+		password: faker.generateRandomPassword(),
 		...overwrite,
 	};
 }
